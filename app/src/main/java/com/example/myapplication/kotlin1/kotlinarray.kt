36af73b574ef1379a5a9728995c5fb04b681e80f
Anochaakimbo <bbcorn123za@gmail.com>
2024-11-20 17:27:08 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/kotlin1/kotlinarray.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication.kotlin1
+
+fun main(){
+    val subjectScore = arrayOf(67,52,73,85,42,78)
+    println("There are " + subjectScore.size+ " Subj in array.")
+    calculateGrade(subjectScore)
+}
+
+fun calculateGrade(scoreArr : Array<Int>){
+    var i : Int = 1
+    var grade : String
+    for(value in scoreArr){
+        grade = when{
+            value <50 -> "F"
+            value <55 -> "D"
+            value <60 -> "D+"
+            value <65 -> "C"
+            value <70 -> "C+"
+            value <75 -> "B"
+            value <80 -> "B+"
+            else -> "A"
+        }
+        println("Grade of subject number $i : $value = $grade")
+        i++
+    }
+}
