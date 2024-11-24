0ef503f31a51479ff06d51e8ceebfdc8f329ad33
Anochaakimbo <bbcorn123za@gmail.com>
2024-11-24 12:28:43 +0700

    ass01Edit

--- app/src/main/java/com/example/myapplication/kotlin1/kotlinarray.kt
+++ app/src/main/java/com/example/myapplication/kotlin1/kotlinarray.kt
@@ -27,7 +27,7 @@ fun calculateGPAX(scoreArr : Array<Int>) {
 
     for(i in 0 .. scoreArr.size-1){
         val (gradeEng,gradeNum) = gradeFormat(scoreArr[i])
-        print("(${gradeNum}*${credits}")
+        print("(${gradeNum}*${credits})")
         if (i != scoreArr.size-1){
             print("+")
         }
